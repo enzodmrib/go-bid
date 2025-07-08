63cabc5fac14ce1e5b29bd2a00fea8bc6a6cf777
enzodmrib <enzorib2@gmail.com>
2025-07-08 15:54:08 -0300

    feat: adds product routes

---
+++ internal/api/product_handlers.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/enzodmrib/go-bid/internal/jsonutils"
+	"github.com/enzodmrib/go-bid/internal/usecase/product"
+	"github.com/google/uuid"
+)
+
+func (api *Api) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	data, problems, err := jsonutils.DecodeValidJson[product.CreateProductReq](r)
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	userID, ok := api.Sessions.Get(r.Context(), "AuthenticatedUserId").(uuid.UUID)
+	if !ok {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "unexpected error, try again later",
+		})
+		return
+	}
+
+	id, err := api.ProductService.CreateProduct(
+		r.Context(),
+		userID,
+		data.ProductName,
+		data.Description,
+		data.Baseprice,
+		data.AuctionEnd,
+	)
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "failed to create product autction try again later",
+		})
+		return
+	}
+
+	jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"message":    "product created successfully",
+		"product_id": id,
+	})
+}
